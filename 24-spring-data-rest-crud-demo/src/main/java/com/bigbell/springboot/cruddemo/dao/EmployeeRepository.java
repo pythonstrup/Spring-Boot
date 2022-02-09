a85155afa908137d27bd61449a4f8416c7108765
BigBell <pythonstrup@gmail.com>
2022-02-09 15:43:23 +0900

    Spring Data JPA/REST

---
+++ 24-spring-data-rest-crud-demo/src/main/java/com/bigbell/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.bigbell.springboot.cruddemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.bigbell.springboot.cruddemo.entity.Employee;
+	
+// @RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	// that's it ... no need to write any code!!
+}
